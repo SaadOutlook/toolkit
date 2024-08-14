017b5934ed258dec854ea945a33acb5e0c6cdeb1
SaadOutlook <saad.siddiqui2014@outlook.com>
2024-08-14 18:08:34 +0500

    Update code.js

--- reco/code.js
+++ reco/code.js
@@ -1,14 +1,16 @@
 import { MongoClient, ServerApiVersion } from 'mongodb';
 const uri = "mongodb+srv://saad:rockstar@cluster0.vj0lzif.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+
 async function run() {
+    const client = new MongoClient(uri, {
+        serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+        }
+      });
+      
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
