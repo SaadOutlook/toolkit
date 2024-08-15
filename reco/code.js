957c249641f53170152c32c81ff1bc8d020b51fc
SaadOutlook <saad.siddiqui2014@outlook.com>
2024-08-15 20:39:22 +0500

    Update code.js

--- reco/code.js
+++ reco/code.js
@@ -1,6 +1,8 @@
 // Assuming you have a Uint8Array representing the SQLite file
 window.onload = async function() {
-    const SQL = await initSqlJs();  // SQL is now a reference to the loaded sql.js module
+    const SQL = await initSqlJs({
+        locateFile: file => `sql-wasm.wasm`
+      });  // SQL is now a reference to the loaded sql.js module
     const fileBuffer = await fetch("articles.db").then(res => res.arrayBuffer());
     const db = new SQL.Database(new Uint8Array(fileBuffer));
     const result = db.exec("SELECT * FROM data;");  // Execute a SELECT statement
