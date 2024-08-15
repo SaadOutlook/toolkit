1d0388d736f35a2f7f60a1dcba8b0b333a6844c3
SaadOutlook <saad.siddiqui2014@outlook.com>
2024-08-15 20:28:13 +0500

    h

---
+++ reco/code.js
@@ -0,0 +1,10 @@
+// Assuming you have a Uint8Array representing the SQLite file
+window.onload = async function() {
+    const SQL = await initSqlJs();  // SQL is now a reference to the loaded sql.js module
+    const fileBuffer = await fetch("articles.db").then(res => res.arrayBuffer());
+    const db = new SQL.Database(new Uint8Array(fileBuffer));
+    const result = db.exec("SELECT * FROM data;");  // Execute a SELECT statement
+    
+    console.log(result);
+};
+
